84bd9d977349c5fcd5e8ac3030080d6d61fdea85
Thirumal <m.thirumal@hotmail.com>
2021-11-06 05:24:54 +0530

    first commit

---
+++ src/main/java/inthirumal/BinanceStakingAlertApplication.java
@@ -0,0 +1,13 @@
+package inthirumal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BinanceStakingAlertApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BinanceStakingAlertApplication.class, args);
+	}
+
+}
---
+++ src/test/java/inthirumal/BinanceStakingAlertApplicationTests.java
@@ -0,0 +1,13 @@
+package inthirumal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BinanceStakingAlertApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
